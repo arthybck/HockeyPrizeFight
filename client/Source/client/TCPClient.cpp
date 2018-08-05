7e35d7a36514bf6f7656e4c086c9434e1cf5acab
arthur <arthurbrunckpro@gmail.com>
2018-08-05 20:41:00 +0200

    settings modified and updated

---
+++ client/Source/client/TCPClient.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TCPClient.h"
+
+TCPClient::TCPClient()
+{
+}
+
+TCPClient::~TCPClient()
+{
+}
---
+++ client/Source/client/clientService.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "clientService.h"
+
+clientService::clientService()
+{
+}
+
+clientService::~clientService()
+{
+}
